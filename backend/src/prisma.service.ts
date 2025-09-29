0137fb32f83389456337afa4ca6f123bff7b6ba8
Zenderg <danil54658798@gmail.com>
2025-09-29 13:55:07 +0300

    remove db connection hooks

--- backend/src/prisma.service.ts
+++ backend/src/prisma.service.ts
@@ -1,15 +1,13 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
-  }
-
-  enableShutdownHooks(app: INestApplication) {
-    process.on('beforeExit', async () => {
-      await app.close();
-    });
+    try {
+      await (this.$connect as () => unknown)();
+    } catch (e) {
+      console.error('Can`t connect to db: ', e);
+    }
   }
 }
