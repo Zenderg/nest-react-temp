c59fd02255cbf386423fd854075fec727f3c7138
Zenderg <danil54658798@gmail.com>
2025-09-29 16:56:18 +0300

    fix prisma deps

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserService } from './user.service';
-import { User } from 'generated/prisma';
+import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
